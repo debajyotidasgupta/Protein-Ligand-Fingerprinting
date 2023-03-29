73750fe1b31108eb1159460d7b148dc6ff79689a
somnathjena2011 <somnathjena.2011@gmail.com>
2023-03-30 01:49:54 +0530

    considering side chains

--- fingerprint/parser.py
+++ fingerprint/parser.py
@@ -59,6 +59,81 @@ class Ligand:
         "
 
 
+class ProteinLigandSideChainComplex:
+    def __init__(self):
+        self.protein = Protein()
+        self.ligand = Ligand()
+
+    def _compute_centroid(self, side_chain_atoms):
+        x = 0.0
+        y = 0.0
+        z = 0.0
+        atom_id = -1
+        for atom in side_chain_atoms:
+            x += atom.x
+            y += atom.y
+            z += atom.z
+            if atom.atom_name == "CB":
+                atom_id = atom.atom_id
+        n_atoms = len(side_chain_atoms)
+        return (x/n_atoms, y/n_atoms, z/n_atoms, atom_id)
+
+    def load_pdb(self, file_path):
+        with open(file_path, "r") as f:
+            lines = f.readlines()
+        
+        side_chain_atoms_dict = {}
+
+        for line in lines:
+            if line.startswith("ATOM") or line.startswith("HETATM"):
+                atom_id = int(line[6:11])
+                atom_name = line[12:16].strip()
+                residue_name = line[17:20].strip()
+                chain_id = line[21]
+                residue_id = int(line[22:26])
+                x = float(line[30:38])
+                y = float(line[38:46])
+                z = float(line[46:54])
+
+                atom = Atom(atom_id, atom_name, residue_name,
+                                chain_id, residue_id, x, y, z)
+
+                is_atom = False
+                if line.startswith("ATOM"):
+                    is_atom = True
+                
+                if is_atom and atom_name.strip()[0] not in ['N', 'C', 'O', 'CA']:
+                    if chain_id not in side_chain_atoms_dict:
+                        side_chain_atoms_dict[chain_id] = {}
+                    if residue_id not in side_chain_atoms_dict[chain_id]:
+                        side_chain_atoms_dict[chain_id][residue_id] = []
+                    side_chain_atoms_dict[chain_id][residue_id].append(atom)
+                else:
+                    if is_atom:
+                        self.protein.add_atom(atom)
+                    else:
+                        self.ligand.add_atom(atom)
+            
+        side_chain_representative_atoms = []
+        for chain_id in side_chain_atoms_dict.keys():
+            for residue_id in side_chain_atoms_dict[chain_id].keys():
+                side_chain_atoms = side_chain_atoms_dict[chain_id][residue_id]
+                x, y, z, atom_id = self._compute_centroid(side_chain_atoms)
+                if atom_id>=0:
+                    atom = Atom(atom_id, "R", residue_name,
+                                chain_id, residue_id, x, y, z)
+                    side_chain_representative_atoms.append(atom)
+        
+        sorter = lambda x : x.atom_id
+        self.protein.atoms = sorted(self.protein.atoms + side_chain_representative_atoms, key = sorter)
+
+    def __repr__(self):
+        return f"ProteinLigandComplex: \n\
+        protein: {self.protein} \n\
+        ligand: {self.ligand} \n\
+        "
+
+
 class ProteinLigandComplex:
     def __init__(self):
         self.protein = Protein()
