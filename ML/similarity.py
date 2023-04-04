e3bb8572fcb88d46355e0c0e9051e9dbccc6fe03
somnathjena2011 <somnathjena.2011@gmail.com>
2023-04-05 03:23:16 +0530

    added cosine similarity

--- ML/similarity.py
+++ ML/similarity.py
@@ -1,14 +1,12 @@
 import os
 import numpy as np
-import pandas as pd
-import requests
-from sklearn.ensemble import RandomForestRegressor
+from numpy.linalg import norm
+import matplotlib.pyplot as plt
+
 import deepchem as dc
 from deepchem.utils import download_url, load_from_disk
-from deepchem.utils.evaluate import Evaluator
 
 from fingerprint import *
-
 
 random_seed = 42
 
@@ -31,7 +29,7 @@ if not os.path.exists(dataset_file):
     print("File downloaded!")
 
 raw_dataset = load_from_disk(dataset_file)
-dataset = raw_dataset[['pdb_id', 'label']]
+dataset = raw_dataset[['pdb_id']]
 
 protein_ligand_dir = os.path.join(data_dir, 'pdb_files')
 if not os.path.isdir(protein_ligand_dir):
@@ -50,11 +48,9 @@ for pdb_id in pdb_ids:
 
 pdb_ids = []
 features = []
-labels = []
 
 for _, row in dataset.iterrows():
     pdb_id = row['pdb_id']
-    label = row['label']
 
     pdb_file = os.path.join(protein_ligand_dir, f"{pdb_id}.pdb")
     feature_output_file = os.path.join(intermediate_dir, f"{pdb_id}.txt")
@@ -77,35 +73,43 @@ for _, row in dataset.iterrows():
 
     pdb_ids.append(pdb_id)
     features.append(fingerprint_array)
-    labels.append(label)
 
-pdb_ids = np.array(pdb_ids)
-max_shape = tuple(max(i) for i in zip(*[a.shape for a in features]))
-padded_arrays = [np.pad(a, [(0, max_shape[0] - a.shape[0]), (0, max_shape[1] - a.shape[1])], 'constant') for a in features]
-padded_arrays = [a.reshape(a.shape[0]*a.shape[1]) for a in padded_arrays]
-features = np.stack(padded_arrays)
-labels = np.array(labels)
+scores = []
 
-dataset = dc.data.NumpyDataset(X=features, y=labels, ids=pdb_ids)
-train_dataset, test_dataset = dc.splits.RandomSplitter().train_test_split(dataset, seed=random_seed)
+def cosine_similarity(feature1, feature2):
+    return np.dot(feature1,feature2)/(norm(feature1)*norm(feature2))
 
-sk_model = RandomForestRegressor(n_estimators=35, max_features="sqrt")
-sk_model.random_state = random_seed
-model = dc.models.SklearnModel(sk_model)
-model.fit(train_dataset)
+for i in range(0, len(features)):
 
-metric = dc.metrics.Metric(dc.metrics.pearson_r2_score)
+    if i==0:
+        print("Processing compounf ", end='')
+    
+    if i%10==0:
+        print(i, end=' ')
+    
+    for j in range(i+1, len(features)):
+        scores.append(features[i], features[j])
+    
+print("Done!")
+print("# of scores : ", len(scores))
 
-train_comparision = list(zip(model.predict(train_dataset), train_dataset.y))
-test_comparision = list(zip(model.predict(test_dataset), test_dataset.y))
+mybins = [ x * 0.01 for x in range(101)]
 
-open(os.path.join(output_dir, 'train_comparision'), 'w').write(str(train_comparision))
-open(os.path.join(output_dir, 'test_comparision'), 'w').write(str(test_comparision))
+fig = plt.figure(figsize=(8,4), dpi=300)
 
-evaluator = Evaluator(model, train_dataset, [])
-train_r2score = evaluator.compute_model_performance([metric])
-print(f'RF Train set R^2 {train_r2score["pearson_r2_score"]}')
+plt.subplot(1, 2, 1)
+plt.title("Distribution")
+plt.hist(scores, bins=mybins)
 
-evaluator = Evaluator(model, test_dataset, [])
-test_r2score = evaluator.compute_model_performance([metric])
-print(f'RF Test set R^2 {test_r2score["pearson_r2_score"]}')
+plt.subplot(1, 2, 2)
+plt.title("Cumulative Distribution")
+plt.hist(scores, bins=mybins, density=True, cumulative=1)
+plt.plot([0,1],[0.95,0.95])
+
+for i in range(21) :
+    thresh = i / 20
+    num_similar_pairs = len([x for x in scores if x >= thresh]) 
+    prob = num_similar_pairs / len(scores) * 100
+    print("%.3f %8d (%8.4f %%)" % (thresh, num_similar_pairs, round(prob,4)))
+
+print("Average:", sum(scores)/len(scores))
