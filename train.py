df06d43b42ac0d79536b80e7762cace43a225e85
Debajyoti Dasgupta <debajyotidasgupta6@gmail.com>
2023-04-05 23:37:13 +0530

    new model

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import argparse
 import torch
+from tqdm import tqdm
 import torch.nn as nn
 import torch.optim as optim
 from sklearn.model_selection import train_test_split
@@ -20,19 +21,31 @@ models_dir = './models'
 max_pdbs = 100
 random_seed = 42
 
+
 def generate_neighbour_fingerprints():
     """
     generate the intermediate fingerprints for max_pdbs pdb ids
     based on neighbourhood fingerprinting
     """
+    # if intermediate directory does not exist, create it
+    if not os.path.isdir(intermediate_dir):
+        os.mkdir(intermediate_dir)
+
+    # if fingerprints directory does not exist, create it
+    if not os.path.isdir(fingerprints_dir):
+        os.mkdir(fingerprints_dir)
+
     dataset_file = os.path.join(data_dir, "pdbbind_core_df.csv.gz")
+
+    print('Building dataset...')
 
     # download the pdbbind dataset
     if not os.path.exists(dataset_file):
         print("File does not exist. Downloading file...")
-        download_url("https://s3-us-west-1.amazonaws.com/deepchem.io/datasets/pdbbind_core_df.csv.gz", dest_dir=data_dir)
+        download_url(
+            "https://s3-us-west-1.amazonaws.com/deepchem.io/datasets/pdbbind_core_df.csv.gz", dest_dir=data_dir)
         print("File downloaded!")
-    
+
     raw_dataset = load_from_disk(dataset_file)
     dataset = raw_dataset[['pdb_id']]
 
@@ -42,8 +55,12 @@ def generate_neighbour_fingerprints():
 
     pdb_ids = dataset['pdb_id']
 
-    # download the pdb files for pdb ids present in pdbbind dataset if not already present 
-    for pdb_id in pdb_ids:
+    # download the pdb files for pdb ids present in pdbbind dataset if not already present
+    print("Downloading pdb files...")
+    for pos, pdb_id in tqdm(list(enumerate(pdb_ids))):
+        if pos > max_pdbs:
+            break
+
         pdb_file = os.path.join(protein_ligand_dir, f"{pdb_id}.pdb")
         if os.path.exists(pdb_file):
             continue
@@ -58,7 +75,7 @@ def generate_neighbour_fingerprints():
         if count >= max_pdbs:
             break
 
-        count+=1
+        count += 1
 
         pdb_file = os.path.join(protein_ligand_dir, f"{pdb_id}.pdb")
         feature_output_file = os.path.join(intermediate_dir, f"{pdb_id}.txt")
@@ -66,7 +83,7 @@ def generate_neighbour_fingerprints():
         # if the intermediate neighbourhood fingerprint already exists continue
         if os.path.exists(feature_output_file):
             continue
-        
+
         else:
             # Create a new instance of the class
             protein_ligand_complex = ProteinLigandSideChainComplex()
@@ -81,11 +98,12 @@ def generate_neighbour_fingerprints():
             # save the intermediate neighbourhood fiingerprint
             np.savetxt(feature_output_file, fingerprint_array, fmt='%1.8f')
 
+
 def train(input_dim=5, hidden_dim=32, num_layers=2, num_heads=5, output_dim=256, epochs=10):
     """
     main code to train the AutoencoderTransformer
     """
-    model = AutoencoderTransformer(input_dim=input_dim, hidden_dim=hidden_dim, 
+    model = AutoencoderTransformer(input_dim=input_dim, hidden_dim=hidden_dim,
                                    num_layers=num_layers, num_heads=num_heads, output_dim=output_dim)
     # train using mean squared error loss
     criterion = nn.MSELoss()
@@ -96,9 +114,10 @@ def train(input_dim=5, hidden_dim=32, num_layers=2, num_heads=5, output_dim=256,
     # load the intermediate neighbourhood fingerprints
     for file in os.listdir(intermediate_dir):
         fingerprints.append(np.loadtxt(os.path.join(intermediate_dir, file)))
-    
+
     # perform a train test split in 80:20 ratio
-    fingerprints_train, fingerprints_test, _, _ = train_test_split(fingerprints, [0 for x in fingerprints], test_size=0.2, random_state=random_seed)
+    fingerprints_train, fingerprints_test, _, _ = train_test_split(
+        fingerprints, [0 for x in fingerprints], test_size=0.2, random_state=random_seed)
 
     train_loader = [torch.from_numpy(x) for x in fingerprints_train]
     train_loader = [torch.unsqueeze(x, 0) for x in train_loader]
@@ -123,11 +142,11 @@ def train(input_dim=5, hidden_dim=32, num_layers=2, num_heads=5, output_dim=256,
             optimizer.step()
 
             running_loss += loss.item()
-            if i % 20== 19:
+            if i % 20 == 19:
                 print('[%d, %5d] training loss: %.3f' %
-                    (epoch + 1, i + 1, running_loss / 20))
+                      (epoch + 1, i + 1, running_loss / 20))
                 running_loss = 0.0
-        
+
         model.eval()
         # EVALUATE
         for i, data in enumerate(test_loader, 0):
@@ -138,7 +157,8 @@ def train(input_dim=5, hidden_dim=32, num_layers=2, num_heads=5, output_dim=256,
                 min_loss = loss
                 print(f"min val loss = {min_loss}")
                 # save the best (minimum validation loss) model so far
-                torch.save(model, os.path.join(models_dir,f"AutoencoderTransformer.pt"))
+                torch.save(model, os.path.join(
+                    models_dir, f"AutoencoderTransformer.pt"))
 
 
 if __name__ == "__main__":
@@ -166,4 +186,5 @@ if __name__ == "__main__":
 
     generate_neighbour_fingerprints()
 
-    train(args.input_dim, args.hidden_dim, args.num_layers, args.num_heads, args.output_dim, args.epochs)
+    train(args.input_dim, args.hidden_dim, args.num_layers,
+          args.num_heads, args.output_dim, args.epochs)
