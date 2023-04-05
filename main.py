a7efdffdb6d961bbc721d929bf0034349651b40b
Debajyoti Dasgupta <debajyotidasgupta6@gmail.com>
2023-04-05 16:05:09 +0530

    downloaded file

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import sys
+from rdkit import Chem
+from rdkit.Chem import MACCSkeys
 from fingerprint import *
 
 if __name__ == '__main__':
@@ -15,24 +17,33 @@ if __name__ == '__main__':
     # Download the PDB file
     download_pdb(pdb, PDB_DATA)
 
-    # Download the SMILES file
-    download_smiles(pdb, SMILES_DATA)
-
     # Load the PDB file
     protein_ligand_complex.load_pdb(os.path.join(PDB_DATA, f'{pdb}.pdb'))
 
-    # Load the SMILES file
-    smiles = load_smiles(os.path.join(SMILES_DATA, f'{pdb}.smiles'))
-
     # # Print the protein fingerprint
-    # fingerprint = NeighbourFingerprint(protein_ligand_complex)
-    # print(fingerprint.get_fingerprint())
+    fingerprint = NeighbourFingerprint(protein_ligand_complex)
+    print(fingerprint.get_fingerprint())
 
-    # # Save the fingerprint
-    # fingerprint.save_fingerprint(args.output)
+    # Save the fingerprint
+    fingerprint.save_fingerprint(
+        os.path.join(OUTPUT_DATA, f'{pdb}.txt'))
 
     # Print the protein
-    # seq=protein2seq(protein_ligand_complex.protein)
-    # vec=KmerVec(0, k=2)
-    # vec.set_kmer_set()
-    # print(vec.vectorize(seq))
+    seq = protein2seq(protein_ligand_complex.protein)
+    vec = KmerVec(0, k=2)
+    vec.set_kmer_set()
+    print(vec.vectorize(seq))
+
+    # Print the ligand Fingerprint
+    try:
+        # Download the SMILES file
+        download_smiles(pdb, SMILES_DATA)
+
+        # Load the SMILES file
+        smiles = load_smiles(os.path.join(SMILES_DATA, f'{pdb}.smiles'))
+
+        mol = Chem.MolFromSmiles(smiles)
+    except:
+        mol = Chem.MolFromPDBFile(os.path.join(PDB_DATA, f'{pdb}.pdb'))
+    fp = MACCSkeys.GenMACCSKeys(mol)
+    print(fp.ToBitString())
