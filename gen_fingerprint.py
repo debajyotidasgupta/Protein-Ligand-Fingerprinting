b340ed3e7b38b13efa03654f7e2090be1404087c
Debajyoti Dasgupta <debajyotidasgupta6@gmail.com>
2023-04-05 18:36:03 +0530

    Working Version ✅

--- gen_fingerprint.py
+++ gen_fingerprint.py
@@ -11,38 +11,34 @@ if __name__ == '__main__':
     SMILES_DATA = config['SMILES_DATA']
     OUTPUT_DATA = config['OUTPUT_DATA']
 
-    # # Create a new instance of the class
-    # protein_ligand_complex = ProteinLigandSideChainComplex()
+    # Create a new instance of the class
+    protein_ligand_complex = ProteinLigandSideChainComplex()
 
-    # # Download the PDB file
-    # download_pdb(pdb, PDB_DATA)
+    # Download the PDB file
+    download_pdb(pdb, PDB_DATA)
 
-    # # Load the PDB file
-    # protein_ligand_complex.load_pdb(os.path.join(PDB_DATA, f'{pdb}.pdb'))
+    # Load the PDB file
+    protein_ligand_complex.load_pdb(os.path.join(PDB_DATA, f'{pdb}.pdb'))
 
-    # # Print the protein fingerprint
+    # Print the protein fingerprint
     fingerprint = NeighbourFingerprint(protein_ligand_complex)
 
     # encode the fingerprint using transformers model to get a constant length feature vector
-    fingerprint = encode(fingerprint.get_fingerprint())
-    print(f"fingerprint shape = {fingerprint.shape}")
-    print("fingerprint:")
-    print(fingerprint)
+    # fingerprint = encode(fingerprint.get_fingerprint())
+    # print(f"fingerprint shape = {fingerprint.shape}")
+    # print(f"fingerprint: {fingerprint}")
 
-    # Save the fingerprint
-    # # # Print the protein fingerprint
-    # fingerprint = NeighbourFingerprint(protein_ligand_complex)
-    # print(fingerprint.get_fingerprint())
+    print(fingerprint.get_fingerprint())
 
     # # Save the fingerprint
     # fingerprint.save_fingerprint(
     #     os.path.join(OUTPUT_DATA, f'{pdb}.txt'))
 
-    # # Print the protein
-    # seq = protein2seq(protein_ligand_complex.protein)
-    # vec = KmerVec(0, k=2)
-    # vec.set_kmer_set()
-    # print(vec.vectorize(seq))
+    # Print the protein
+    seq = protein2seq(protein_ligand_complex.protein)
+    vec = KmerVec(0, k=2)
+    vec.set_kmer_set()
+    print(vec.vectorize(seq))
 
     # Print the ligand Fingerprint
     fp = get_ligand_fingerprint(pdb=pdb,
